4773b97d1f1808fc31027e3eb489b2ade61b83de
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2026-08-08 08:31:21 +0300

    Aspect-Oriented Programming in Spring

--- section_10/jobportal/src/main/java/com/eazybytes/jobportal/aspect/LoggingAndPerformanceAspect.java
+++ section_10/jobportal/src/main/java/com/eazybytes/jobportal/aspect/LoggingAndPerformanceAspect.java
@@ -16,16 +16,21 @@ public class LoggingAndPerformanceAspect {
     // @Around("@annotation(com.eazybytes.jobportal.aspects.LogAspect)")
     @Around("execution(* com.eazybytes.jobportal..*.*(..))")
     public Object  logAndMeasureExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
         long startTime = System.currentTimeMillis();
         String methodName = joinPoint.getSignature().toShortString();
         Object[] methodArgs = joinPoint.getArgs();
+
         log.info("➡️ Entering method: {}", methodName);
         log.info("📥 Arguments: {}", Arrays.toString(methodArgs));
+
         // Proceed with actual business method
         Object result = joinPoint.proceed();
         long executionTime = System.currentTimeMillis() - startTime;
+
         log.info("✅ Method executed successfully: {}", methodName);
         log.info("⏱ Execution time: {} ms", executionTime);
+
         return result;
     }
 
